d4c16903d8c9be00a63c4eabdeb810aa16e3e0de
Nima_Torabi <nimatorabi.t@gmail.com>
2024-04-29 22:29:29 +0330

    me.jpg added to wwwroot

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -6,19 +6,10 @@ namespace DevCard_MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
-
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
