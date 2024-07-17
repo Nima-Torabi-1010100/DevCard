7fc0c189d79c27e2ac506e5ce9ad6f9a81ec014f
Nima_Torabi <nimatorabi.t@gmail.com>
2024-07-18 01:42:05 +0330

    project section was added to view component

---
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var _projects = new List<Project>()
+            {
+                new Project(1,"Taxi","Lorem ipsum dolor sit amet.","SNAPP","project-1.jpg"),
+                new Project(2,"University","Lorem ipsum dolor sit amet.","TAU","project-2.jpg"),
+                new Project(3,"Airport","Lorem ipsum dolor sit amet.","Turkish Airlines","project-4.jpg"),
+                new Project(4,"Gym","Lorem ipsum dolor sit amet.","Mola Gym","project-4.jpg"),
+            };
+            return View("Projects",_projects);
+        }
+    }
+}
