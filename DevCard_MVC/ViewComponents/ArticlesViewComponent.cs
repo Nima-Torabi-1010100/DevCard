b2bb204cebd57be01aebb78a2319934ea5ecda6e
Nima_Torabi <nimatorabi.t@gmail.com>
2024-07-18 13:03:18 +0330

    article section moved to view component

---
+++ DevCard_MVC/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var _articles = new List<Article>()
+            {
+                new Article(1,"Top 3 JavaScript Frameworks","Lorem ipsum dolor sit amet.","blog-post-thumb-card-1.jpg"),
+                new Article(2,"About Remote Working","Lorem ipsum dolor sit amet.","blog-post-thumb-card-2.jpg"),
+                new Article(3,"A Full Guide to Becoming a Full-Stack Developer","Lorem ipsum dolor sit amet.","blog-post-thumb-card-3.jpg"),
+                new Article(4,"A Guide to Becoming a Web Developer","Lorem ipsum dolor sit amet.","blog-post-thumb-card-4.jpg"),
+            };
+            return View("_Articles", _articles);
+        }
+    }
+}
--- DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -12,10 +12,10 @@ namespace DevCard_MVC.ViewComponents
             {
                 new Project(1,"Taxi","Lorem ipsum dolor sit amet.","SNAPP","project-1.jpg"),
                 new Project(2,"University","Lorem ipsum dolor sit amet.","TAU","project-2.jpg"),
-                new Project(3,"Airport","Lorem ipsum dolor sit amet.","Turkish Airlines","project-4.jpg"),
+                new Project(3,"Airport","Lorem ipsum dolor sit amet.","Turkish Airlines","project-3.jpg"),
                 new Project(4,"Gym","Lorem ipsum dolor sit amet.","Mola Gym","project-4.jpg"),
             };
-            return View("Projects",_projects);
+            return View("_Projects",_projects);
         }
     }
 }
